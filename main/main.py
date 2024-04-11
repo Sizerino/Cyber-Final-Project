d0b805148d659f9ae8c3a2e0192169fe4706c8d5
Sizerino <myultisreadylol@gmail.com>
2024-04-11 17:11:18 +0300

    Updated main.py

--- main/main.py
+++ main/main.py
@@ -2,17 +2,22 @@ import sys
 import platform
 import subprocess
 
+
 def main():
     if platform.system() == "Windows":
-        Process = subprocess.Popen('powershell.exe -ExecutionPolicy RemoteSigned -file "windows.ps1"', stdout = sys.stdout)
-        Process.communicate()
+        process = subprocess.Popen('powershell.exe -ExecutionPolicy RemoteSigned -file "windows.ps1"',
+                                   stdout=sys.stdout)
+        process.communicate()
         print("")
+
     elif platform.system() == "Linux":
-        Process = subprocess.Popen(["/bin/bash", "./main/linux.bash"], stdout = sys.stdout)
-        Process.communicate()
+        process = subprocess.Popen(["/bin/bash", "./main/linux.bash"], stdout=sys.stdout)
+        process.communicate()
         print("")
+
     else:
         print("Can't identify OS")
-    
+
+
 if __name__ == "__main__":
     main()
