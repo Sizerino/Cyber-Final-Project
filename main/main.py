0b0156ecc4303006f9ea0a9ad0ebd205927335c6
Sizerino <myultisreadylol@gmail.com>
2024-06-08 17:29:33 +0300

    Updated main.py

--- main/main.py
+++ main/main.py
@@ -9,19 +9,12 @@ def main():
             './powershell.exe -ExecutionPolicy RemoteSigned -file "windows.ps1"',
             stdout=sys.stdout
         )
-        process.communicate()
-        print("")
-
-    elif platform.system() == "Linux":
-        process = subprocess.Popen(
-            ["/bin/bash", "./linux.bash"],
-            stdout=sys.stdout
-        )
+        
         process.communicate()
         print("")
 
     else:
-        print("Can't identify OS")
+        print("The Host OS Is Not Windows")
 
 
 if __name__ == "__main__":
