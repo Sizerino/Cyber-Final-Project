a9dd7f68989f13998587ac84683ccf81a5d02352
Sizerino <myultisreadylol@gmail.com>
2024-06-08 17:11:00 +0300

    Updated portScanner.py

--- testing/portScanner.py
+++ testing/portScanner.py
@@ -1,8 +1,8 @@
 import socket
- 
+
 ip = "192.168.1.132"
 openPorts = []
- 
+
 for port in range(65535):
     sock = socket.socket()
     sock.settimeout(0.001)
@@ -10,8 +10,8 @@ for port in range(65535):
     if attempt == 0:
         print("The Server's COULD be using the port:", port)
         openPorts.append(port)
-        
+
     else:
         print("The Server is NOT using the port:", port)
-        
+
 sock.close()
