ded72392d08a38b11e060a5b370f40f02fc88bc8
Sizerino <myultisreadylol@gmail.com>
2024-06-08 17:30:19 +0300

    New directory "procedure"

--- procedure/portScanner.py
+++ procedure/portScanner.py
@@ -1,12 +1,13 @@
 import socket
 
-ip = "192.168.1.132"
+ip = "127.0.0.1"
 openPorts = []
 
 for port in range(65535):
     sock = socket.socket()
     sock.settimeout(0.001)
     attempt = sock.connect_ex((ip, port))
+
     if attempt == 0:
         print("The Server's COULD be using the port:", port)
         openPorts.append(port)
