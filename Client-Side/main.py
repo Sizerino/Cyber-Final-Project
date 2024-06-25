cae04811ebf9a26173dc0f6939e3c254bd95b2b4
Sizerino <myultisreadylol@gmail.com>
2024-06-25 19:14:31 +0300

    Updated main.py

--- Client-Side/main.py
+++ Client-Side/main.py
@@ -11,7 +11,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.eval("tk::PlaceWindow . center")
-        self.geometry("900x450")
+        self.geometry("950x550")
         self.title("Stack Buffer Overflow")
         customtkinter.set_appearance_mode("dark")
         # self.wm_iconbitmap("./assets/")
@@ -66,32 +66,34 @@ class MainFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.button = customtkinter.CTkButton(
-            master=self,
-            text="Start Stage 1",
-            command=lambda: action(1, [None])
-        )
-        self.button.grid(padx=5, pady=5, row=0, column=0, columnspan=2)
-
         self.label = customtkinter.CTkLabel(
             master=self,
             text=(
-                "STATUS: Waiting For User Action..."  # \n
-                # "Enter The Following Values: [Server IP | Server Port]\n"
-                # "After That, Start Stage 1."
+                "INFO: Waiting For User Action...\n"
+                "Enter The Following Server Values: [IP | Port]\n"
+                "After That, Start The First Stage."
             )
         )
-        self.label.grid(padx=5, row=1, column=0, columnspan=2)
+        self.label.grid(padx=5, pady=5, row=0, column=0, columnspan=2)
 
         self.input1 = customtkinter.CTkEntry(
-            master=self
+            master=self,
+            justify="center"
         )
-        # self.input1.grid(sticky="we", padx=5, pady=5, row=2, column=0)
+        self.input1.grid(sticky="we", padx=5, pady=5, row=1, column=0)
 
         self.input2 = customtkinter.CTkEntry(
-            master=self
+            master=self,
+            justify="center"
         )
-        # self.input2.grid(sticky="we", padx=5, pady=5, row=2, column=1)
+        self.input2.grid(sticky="we", padx=5, pady=5, row=1, column=1)
+
+        self.button = customtkinter.CTkButton(
+            master=self,
+            text="Start Stage 1",
+            command=lambda: action(1, [self.input1.get(), self.input2.get()])
+        )
+        self.button.grid(padx=5, pady=5, row=2, column=0, columnspan=2)
 
         self.console = customtkinter.CTkTextbox(
             master=self
@@ -102,16 +104,44 @@ class MainFrame(customtkinter.CTkFrame):
         self.columnconfigure((0, 1), weight=1)
 
         def action(stage, args):
+            global ip
+            global port
+
             if stage == 1:
-                threadedfunction(fuzzer.fuzz, ("192.168.31.129", 9999))
+                ip = args[0]
+                port = int(args[1])
+
+                threadedfunction(
+                    fuzzer.fuzz,
+                    (
+                        ip,
+                        port
+                    )
+                )
+
                 self.button.configure(state=customtkinter.DISABLED)
 
                 self.label.configure(
                     text="STATUS: Fuzzing..."
                 )
 
+                self.button.configure(
+                    state=customtkinter.DISABLED,
+                    text="Running Stage..."
+                )
+
+                self.input1.grid_forget()
+                self.input2.grid_forget()
+
             if stage == 2:
-                threadedfunction(buffer.buff, ("192.168.31.129", 9999, args[0]))
+                threadedfunction(
+                    buffer.buff,
+                    (
+                        ip,
+                        port,
+                        args[0]
+                    )
+                )
 
                 self.button.configure(
                     state=customtkinter.DISABLED,
@@ -126,8 +156,8 @@ class MainFrame(customtkinter.CTkFrame):
                 threadedfunction(
                     payload.exploit,
                     (
-                        "192.168.31.129",
-                        9999,
+                        ip,
+                        port,
                         args[0],
                         args[1],
                         args[2]
@@ -156,12 +186,15 @@ class MainFrame(customtkinter.CTkFrame):
             if "Couldn't" in stdout:
                 self.button.configure(
                     state=customtkinter.NORMAL,
-                    text="Stage Stopped, Retry?"
+                    text="Retry Previous Stage"
                 )
 
                 self.label.configure(
-                    text="ERROR: Stage Cannot Proceed."
+                    text="ERROR: Stage Could Not Proceed."
                 )
+
+                self.input1.grid(sticky="we", padx=5, pady=5, row=1, column=0)
+                self.input2.grid(sticky="we", padx=5, pady=5, row=1, column=1)
 
             if "end of life" in stdout:
                 fuzzsize = int(
@@ -200,11 +233,16 @@ class MainFrame(customtkinter.CTkFrame):
                     )
                 )
 
-                self.input1.grid(sticky="we", padx=5, pady=5, row=2, column=0)
-                self.input2.grid(sticky="we", padx=5, pady=5, row=2, column=1)
+                self.input1.grid(sticky="we", padx=5, pady=5, row=1, column=0)
+                self.input1.delete(0, customtkinter.END)
+                self.input2.grid(sticky="we", padx=5, pady=5, row=1, column=1)
+                self.input2.delete(0, customtkinter.END)
 
         sys.stdout.write = consoleoutput
 
+
+ip: str
+port: int
 
 app = App()
 app.mainloop()
